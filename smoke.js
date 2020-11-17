18f4486d2d5604f3a1e3e8b7e61bf18989af58f6
smoke <smokepei@gmail.com>
2020-11-17 20:17:59 +0800

    miss you

--- smoke.js
+++ smoke.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('codeing happy');
+  response.writeHead(200, {'Content-Type': 'text/html; charset="utf-8"'});
+  response.end('想你的夜');
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
