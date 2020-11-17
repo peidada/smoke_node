fd493bdc09ec273afc5c2c4c196f4c5616feba0b
smoke <smokepei@gmail.com>
2020-11-17 19:57:26 +0800

    ceshi

--- smoke.js
+++ smoke.js
@@ -1,12 +1,7 @@
 var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World');
-  response.end('Hello World');
-  response.end('Hello World');
-  response.end('Hello World');
-  response.end('Hello World');
+  response.end('Hello Wo2222rld');
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
-
