7a80313b440574384b90ce4259b968d6ea356a27
smoke <smokepei@gmail.com>
2020-11-19 18:23:33 +0800

    change title

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/string', async (ctx, next) => {
 
 router.get('/json', async (ctx, next) => {
   let data = {
-    title: 'koa2 jsonss'
+    title: '年轻人不讲武德，耗子尾汁'
   }
   ctx.body = common.jsonFun(200, data, 'success');
 })
